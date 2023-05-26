1e9c8f4b7b39a09053ccada4616c92809cebd2b3
Ricardo Santiago <ricardosantiago98@gmail.com>
2023-05-26 15:09:16 -0300

    Update Lucro por mes.sql

--- Lucro por mes.sql
+++ Lucro por mes.sql
@@ -2,4 +2,5 @@
 
 SELECT MONTH(job_date) AS mes, SUM(CAST(job_value AS DECIMAL(10, 2))) - SUM(CAST(tasker_payout AS DECIMAL(10, 2))) AS lucro
 FROM Base_teste
+WHERE job_state = 'completed'
 GROUP BY MONTH(job_date)
